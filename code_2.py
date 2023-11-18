f6e8d6ef2bb31498ea9b7d64f2597a0622573ea7
haldola05 <146481065+haldola05@users.noreply.github.com>
2023-11-18 12:07:01 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    const data = generateRandomData();
+    }
+
+    data.forEach(item => {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+    });
+function generateRandomData() {
+}
+main();
+}
+function main() {
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+
+    return data;
